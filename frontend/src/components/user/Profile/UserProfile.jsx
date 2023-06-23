2665ea85ac646087e772b0bad007ce36662942e0
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2023-06-23 18:12:29 +0530

    june 23

--- frontend/src/components/user/Profile/UserProfile.jsx
+++ frontend/src/components/user/Profile/UserProfile.jsx
@@ -1,76 +1,127 @@
-
-
-import React from 'react';
-import Navbar from '../Navbar';
-import { Box, Container, Grid, Card, Typography, CardContent, CardMedia, Avatar } from '@mui/material';
-import { styled } from '@mui/system';
-import { Edit, Facebook, Twitter, Instagram } from '@mui/icons-material';
+import React from "react";
+import ProfileCard from "../../customItems/ProfileCard";
+import Navbar from "../Navbar";
+import {
+  Box,
+  Container,
+  Grid,
+  Card,
+  Typography,
+  CardContent,
+  CardMedia,
+  Avatar,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import { Edit, Facebook, Twitter, Instagram } from "@mui/icons-material";
 
 const GradientCustomCard = styled(Card)`
-  background: linear-gradient(to right bottom, rgba(246, 211, 101, 1), rgba(253, 160, 133, 1));
+  background: linear-gradient(
+    to right bottom,
+    rgba(246, 211, 101, 1),
+    rgba(253, 160, 133, 1)
+  );
   border-radius: 0.5rem;
 `;
 
 const UserProfile = () => {
   return (
     <>
-    <Navbar/>
-    
-    
+      <Navbar />
+      <Box
+        sx={{ height: "100%", backgroundColor: "#f4f5f7", marginTop: "73px" }}
+      >
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="center"
+          sx={{ height: "100%" }}
+        >
+          <Box
+            item
+            maxWidth="1250px"
+            maxHeight="1000px"
+            borderRadius="10px"
+            sx={{ backgroundColor: "black" }}
+          >
+            <Grid
+              xs={12}
+              container
+              justifyContent="space-between"
+              sx={{ padding: "0" }}
+            >
+              <Grid>
+                <Box
+                  item
+                  sx={{
+                    width: "300px",
+                    height: "500px",
+                    background:
+                      "linear-gradient(to right bottom, #2684FF,#79F2C0)",
+                  }}
+                >
+                  <Grid
+                    container
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                  >
+                    <Avatar
+                      alt="Profile"
+                      src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+                      sx={{ width: 120, height: 120, my: 3 }}
+                    />
+
+                    
+                  </Grid>
+                  <Grid>
+                     <Typography variant="h5" component="div">
+                      User Name
+                    </Typography>
+                    <Typography variant="body1" color="textSecondary">
+                      Web Designer
+                    </Typography>
+                    </Grid>
+                    {/* <Grid>
+                      <Edit/>
+                    </Grid> */}
+                </Box>
+              </Grid>
+              <Grid>
+                <Box
+                  item
+                  className="box2"
+                  sx={{
+                    maxWidth: "900px",
+                    maxHeight: "1000px",
+                    background:
+                    "linear-gradient(to right bottom, #9CBFED,#9BE9C9)",
+                  }}
+                >
+                 <Grid container sx={{justifyContent:"space"}}  >
+                 
+                  <ProfileCard/>
+                  <ProfileCard/>
+                  <ProfileCard/>
+                  <ProfileCard/>
+                 
+                
+
+               
+                 </Grid>
+
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+      </Box>
     </>
   );
 };
 
 export default UserProfile;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <Box className="vh-100" sx={{ backgroundColor: '#f4f5f7',marginTop:"73px" }}>
+{
+  /* <Box className="vh-100" sx={{ backgroundColor: '#f4f5f7',marginTop:"73px" }}>
       <Container className="py-5" sx={{ height: '100%' }}>
         <Grid container justifyContent="center" alignItems="center" sx={{ height: '100%' }}>
           <Grid item lg={6} mb={4} mb-lg={0}>
@@ -129,5 +180,5 @@ export default UserProfile;
           </Grid>
         </Grid>
       </Container>
-    </Box> */}
-
+    </Box> */
+}
