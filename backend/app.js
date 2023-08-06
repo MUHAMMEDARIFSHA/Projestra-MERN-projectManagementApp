87c661337fb6c6958a0ff39e936adf0ec43fffbb
MUHAMMED ARIFSHA <muhammedarifsha2001@gmail.com>
2023-08-06 20:46:11 +0530

    some updations and the backend db code changed

--- backend/app.js
+++ backend/app.js
@@ -47,6 +47,10 @@ app.use("/admin",adminRouter)
 
 // listening to the port
 const PORT = process.env.PORT || 5002;
-const server = app.listen(PORT, () => console.log(`server conected to ${PORT}`));
+// const server = app.listen(PORT, () => console.log(`server conected to ${PORT}`));
+// io.attach(server)
+
+const server = app.listen(PORT, db.connect().then(() => console.log(`server conected to ${PORT}`)));
 io.attach(server)
+
 
--- backend/database/mongoDb.js
+++ backend/database/mongoDb.js
@@ -1,21 +1,42 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const dbUrl = process.env.MONGO_URL;
+
+// mongoose
+//   .connect(dbUrl, {})
+//   .then(() => {
+//     console.log(`Connected to MongoDB `);
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// module.exports = db;
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbUrl = process.env.MONGO_URL;
 
-mongoose
+const connect = ()=>{
+return mongoose
   .connect(dbUrl, {})
   .then(() => {
     console.log(`Connected to MongoDB `);
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
-  });
+  })};
 
-const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-module.exports = db;
+
+module.exports = {connect};
+
 
 
